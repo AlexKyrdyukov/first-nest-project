0f3355d80a53f7d88084f7edb0c7c27afd4cda15
Fusion <fusion@Fusions-Mac-mini.local>
2023-03-14 20:37:49 +0300

    create new app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
-import { Module } from '@nestjs/common';
+import { LoggerMiddleware } from './cats/logger.middleware';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
 
+// @Module({
+//   imports: [CatsModule],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CatsModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('cats');
+  }
+}
---
+++ src/cats/services/cats.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { Cat } from 'src/cats/types/interfaces/cat.interface';
+
+@Injectable()
+export class CatsService {
+  private readonly cats: Cat[] = [];
+
+  create(cat: Cat) {
+    this.cats.push(cat);
+  }
+
+  findAll(): Cat[] {
+    return this.cats;
+  }
+}
